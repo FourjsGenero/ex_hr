4bd7559f48e37061b22c7e9d2c3ab5c898e6f53c
MoHo <MoHo@4js.com>
2023-04-17 14:08:43 +1000

    Updates to hr.xcf config for GAR deployment

--- distbin/deploy.sh
+++ distbin/deploy.sh
@@ -1,4 +1,7 @@
 case $# in
+  0)  echo "use: $(basename $0) gar_archive" 1>&2
+      exit 1
+      ;;
   1)  p_gar="$1"
       gasadmin gar --disable-archive $p_gar
       gasadmin gar --undeploy-archive $p_gar
